ba221a11191c9e05384be110fb79e6d75a96ea69
genecarles <genecarles@gmail.com>
2026-04-24 18:08:17 +0200

    ?p=

--- functions/saludo.js
+++ functions/saludo.js
@@ -17,10 +17,3 @@ export async function onRequest(context) {
   return new Response(fecha || "2026-01-01");
 }
 
-export async function onRequest(context) {
-  // Leemos la clave específica del cliente
-  const fecha = await context.env.KV_DATA.get("OLEYAJI/DATA_CADUCITAT");
-  
-  // Devolvemos el dato
-  return new Response(fecha || "2026-01-01");
-}
