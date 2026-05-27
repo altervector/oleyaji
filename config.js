428ebe89c03c6168db199f831f0cf9f78086343b
genecarles <genecarles@gmail.com>
2026-05-27 10:38:21 +0200

    Fix typo in QR image path

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const CONFIG = {
     BLOC2:           "images/tapes.jpg",
     BLOC3:           "images/postres.jpg",
     BLOC4:           "images/begudes.png",
-    QR:              "/qr/qr-oleiaji.png",
+    QR:              "/qr/qr-oleyaji.png",
 
     // 2.3 RUTES Textos
     HERO_BOTO:       "Descúbrenos",
